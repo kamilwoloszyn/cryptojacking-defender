6b9d8d8d784a5e303e93baa5202d010b7d8533b3
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-08-09 21:24:29 +0200

    Added main

---
+++ cmd/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"log"
+	"sync"
+
+	"github.com/caarlos0/env"
+	"github.com/kamilwoloszyn/cryptojacking-defender/config"
+	"github.com/kamilwoloszyn/cryptojacking-defender/external/chrome"
+	"github.com/kamilwoloszyn/cryptojacking-defender/external/tcpdump"
+	"github.com/kamilwoloszyn/cryptojacking-defender/utils/cleanup"
+)
+
+func main() {
+	var (
+		wg sync.WaitGroup
+	)
+	ctx := context.Background()
+	cfg := config.Config{}
+
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatalf("[FATAL]: Cannot parse config: %s. Unexpected behavior may occur.", err)
+	}
+	log.Println("[INFO]: Config loaded succesfully")
+	tcpDumpCtx, cancelTcpDump := context.WithCancel(ctx)
+	tcpDump := tcpdump.New(cfg.NetworkInterface, cfg.TcpDumpFilePath)
+	log.Println("[INFO]: Starting capturing ...\n [WARNING]: Password may be required for sudo")
+	tcpDump.Capture(tcpDumpCtx, &wg)
+	log.Println("[INFO]: Waiting for a browser .. ")
+	chromeBrowser := chrome.New(cfg.BrowserSSLFilePath)
+	if err := chromeBrowser.Run(); err != nil {
+		log.Fatalf("[FATAL]: Couldn't run chrome browser: %s. Exiting.", err)
+		return
+	}
+	log.Println("[INFO]: Browser closed. Looking for dump program ...")
+	cancelTcpDump()
+	wg.Wait()
+
+	//cleanup
+	log.Println("[INFO]: Cleaning files ...")
+	if err := cleanup.RemoveAsRoot(cfg.BrowserSSLFilePath, cfg.TcpDumpFilePath); err != nil {
+		log.Fatalf("[FATAL]: Cannot make cleanup: %s", err.Error())
+	}
+
+}
