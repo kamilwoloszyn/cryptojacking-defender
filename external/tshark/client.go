d2fe5e1b6f3e810d27287642160fb896a5307482
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-08-15 22:07:18 +0200

    Added tshark client

--- external/tshark/client.go
+++ external/tshark/client.go
@@ -1,9 +1,29 @@
 package tshark
 
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+//Client Tshark is used for decrypt captured pcap files.
 type Tshark struct {
 	SslKeysPath string
 }
 
+func New(
+	sslKeyPath string,
+) *Tshark {
+	return &Tshark{
+		SslKeysPath: sslKeyPath,
+	}
+}
+
 func (t *Tshark) Decrypt(pcapLocation string) {
-	//cmd := exec.Command("/bin/sh", "-c", "tshark ")
+	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("tshark -r %s -o \"ssl.keylog_file: %s\" -Px -Y http", pcapLocation, t.SslKeysPath))
+	tsharkData, err := cmd.Output()
+	if err != nil {
+		log.Fatalf("[FATAL]: Cannot open decryption program: %s", err)
+	}
+	log.Printf("Ok, got data: %d", len(tsharkData))
 }
