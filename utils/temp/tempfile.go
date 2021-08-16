4218b7b79c9d26babea0c634a0498469630803c3
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-08-16 22:42:16 +0200

    Added tempfile gen func

---
+++ utils/temp/tempfile.go
@@ -0,0 +1,14 @@
+package temp
+
+import "os/exec"
+
+// CreateTempFile creates new plain file and returns its path
+
+func CreateTempFile() (string, error) {
+	cmd := exec.Command("tempfile")
+	tempPath, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(tempPath), nil
+}
