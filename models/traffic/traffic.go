52c65d2ecd5e0587d9c5f2c45ffa909f3ff07460
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-08-19 23:30:41 +0200

    Moved struct

--- models/traffic/traffic.go
+++ models/traffic/traffic.go
@@ -7,29 +7,7 @@ import (
 	"reflect"
 )
 
-type Layers struct {
-	IPSrc            string `json:"ip.src"`
-	IPDst            string `json:"ip.dst"`
-	TLSDecryptedBody string `json:"-"`
-	TLSContentType   string `json:"tls.record.content_type"`
-	Frame            string `json:"frame"`
-	FrameNumber      int32  `json:"-"`
-}
-type Source struct {
-	Layers Layers `json:"layers"`
-}
-type Traffic struct {
-	Index       string `json:"_index"`
-	TypeTraffic string `json:"_type"`
-	Score       string `json:"_score"`
-	Source      Source `json:"_source"`
-}
-
-func New() *Traffic {
-	return &Traffic{}
-}
-
-// ParseFromJSON loads a traffic from a text file, that contain json struct
+// ParseFromJSON loads a traffic from a text file, which contains json struct
 // terminal command tshark -r traffic.pcap -o "tls.keylog_file: keys2.txt" -Y tls -Px -T json -e ip.src -e ip.dst -e tls.record.content_type -e data-text-lines -e tls.record.content_type -e frame
 func ParseFromJSON(jsonFileAbsPath string) ([]Traffic, error) {
 	traffic := []Traffic{}
