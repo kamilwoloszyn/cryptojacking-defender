301c1957de5941469834f1c7e385484724c88da9
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-08-19 23:31:09 +0200

    Moved struct

--- models/traffic/decrypted.go
+++ models/traffic/decrypted.go
@@ -1,16 +1,7 @@
 package traffic
 
-type IPAddr string
-
-// TrafficDecrypted is for decrypted version of tshark, because tshark doesn't provide dump to json
-type TrafficDecrypted struct {
-	SrcIP        IPAddr
-	DstIP        IPAddr
-	DecryptedTLS string
-	FrameNumber  int32
-}
-
 func ParseFromTsharkOutputFile(absPath string) ([]TrafficDecrypted, error) {
 	trafficDecrypted := []TrafficDecrypted{}
+
 	return trafficDecrypted, nil
 }
