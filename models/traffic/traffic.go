1392863f207eb477cad1a62002085e0d237577c8
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-08-23 22:58:48 +0200

    pulled structs into traffic

--- models/traffic/traffic.go
+++ models/traffic/traffic.go
@@ -4,11 +4,37 @@ import (
 	"encoding/json"
 	"errors"
 	"os"
-	"reflect"
 )
 
+type IPAddr string
+
+// Layers contains needed informations about packet.
+type Layers struct {
+	IPSrc             string   `json:"ip.src"`
+	IPDst             string   `json:"ip.dst"`
+	TLSContentType    int32    `json:"tls.record.content_type"`
+	FrameNumber       int32    `json:"frame.number"`
+	FrameLength       int32    `json:"frame.len"`
+	FrameTime         string   `json:"frame.time"`
+	FrameTimeRelative float32  `json:"frame.time_relative"`
+	TextData          []string `json:"text"`
+}
+
+// Source contains nested Layer body
+type Source struct {
+	Layers Layers `json:"layers"`
+}
+
+// Traffic contains all info about packet
+type Traffic struct {
+	Index       string  `json:"_index"`
+	TypeTraffic string  `json:"_type"`
+	Score       *string `json:"_score"`
+	Source      Source  `json:"_source"`
+}
+
 // ParseFromJSON loads a traffic from a text file, which contains json struct
-// terminal command tshark -r traffic.pcap -o "tls.keylog_file: keys2.txt" -Y tls -Px -T json -e ip.src -e ip.dst -e tls.record.content_type -e data-text-lines -e tls.record.content_type -e frame
+// terminal command tshark -r traffic.pcap -o "tls.keylog_file: keys2.txt" -Y tls -Px -T json -e ip.src -e ip.dst -e tls.record.content_type -e data-text-lines -e tls.record.content_type -e frame.number -e frame.len -e frame.time -e frame.time_relative -e text
 func ParseFromJSON(jsonFileAbsPath string) ([]Traffic, error) {
 	traffic := []Traffic{}
 	if len(jsonFileAbsPath) == 0 {
@@ -22,19 +48,4 @@ func ParseFromJSON(jsonFileAbsPath string) ([]Traffic, error) {
 		return []Traffic{}, err
 	}
 	return traffic, nil
-}
-
-// MergeWithDecryptedTraffic performs to adding decrypted tls body to traffic. Probably a heavy operation due to copy, but indexing is needed.
-func MergeWithDecryptedTraffic(t []Traffic, td []TrafficDecrypted) ([]Traffic, error) {
-	if len(td) == 0 {
-		return t, errors.New("decrypted traffic is empty")
-	}
-	for trafficElementIndex, trafficElement := range t {
-		for trafficDecryptedElementIndex, trafficDecryptedElement := range td {
-			if equal := reflect.DeepEqual(trafficElement.Source.Layers.FrameNumber, trafficDecryptedElement.FrameNumber); equal {
-				t[trafficElementIndex].Source.Layers.TLSDecryptedBody = td[trafficDecryptedElementIndex].DecryptedTLS
-			}
-		}
-	}
-	return t, nil
 }
