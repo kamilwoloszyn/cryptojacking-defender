c42d9d1fb3ca8c86d56846c04afc6c68c08057b8
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-08-23 22:55:55 +0200

    Added wordlist

---
+++ models/wordlist/wordlist.go
@@ -0,0 +1,50 @@
+package wordlist
+
+import (
+	"bufio"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+// Wordlist contains list of reqest or response pattern
+type WordList struct {
+	Req []string
+	Res []string
+}
+type switchFlag string
+
+const (
+	statusRequest  switchFlag = "req"
+	statusResponse switchFlag = "res"
+)
+
+func (w *WordList) ParseFromFile(absPath string) (*WordList, error) {
+	var flag switchFlag
+	wList := WordList{
+		Req: []string{},
+		Res: []string{},
+	}
+	file, err := os.Open(absPath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Loading wordlist from file")
+	}
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+	for fileScanner.Scan() {
+		switch word := fileScanner.Text(); word {
+		case "req:":
+			flag = statusRequest
+		case "res:":
+			flag = statusResponse
+		default:
+			if flag == statusRequest {
+				wList.Req = append(wList.Req, word)
+			}
+			if flag == statusResponse {
+				wList.Res = append(wList.Res, word)
+			}
+		}
+	}
+	return &wList, nil
+}
