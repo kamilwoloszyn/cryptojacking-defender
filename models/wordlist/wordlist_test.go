6c833f8d604fac943c00f8ec24406a0f9c023328
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-09-01 23:05:59 +0200

    Added tests to wordlist

---
+++ models/wordlist/wordlist_test.go
@@ -0,0 +1,50 @@
+package wordlist_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/kamilwoloszyn/cryptojacking-defender/models/wordlist"
+)
+
+func TestParseWordlistFromFile(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		arg      string
+		expected *wordlist.WordList
+	}{
+		{
+			desc: "Correct wordlist path",
+			arg:  "/home/kamil/Projects/cryptojacking-defender/static/wordlist_test.txt",
+			expected: &wordlist.WordList{
+				Req: []string{
+					"nonce",
+					"result",
+				},
+				Res: []string{
+					"hashes",
+					"blob",
+					"job",
+					"hash",
+				},
+			},
+		},
+		{
+			desc:     "Wrong wordlist path",
+			arg:      "/home/aaa/aaa.txt",
+			expected: nil,
+		},
+	}
+
+	for i, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			if result, err := wordlist.ParseFromFile(tC.arg); !reflect.DeepEqual(result, tC.expected) {
+				t.Error(
+					fmt.Sprintf("Got %v, but expected %v with err :%s", result, tC.expected, err.Error()),
+				)
+			}
+			fmt.Printf("[%d/%d]: Success\n", i+1, len(testCases))
+		})
+	}
+}
