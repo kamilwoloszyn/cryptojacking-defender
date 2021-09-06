ab22d036dac78757a3cceda1a7a62e84817bd8cd
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-09-06 11:04:55 +0200

    Refactored test

--- models/tstraining/model.go
+++ models/tstraining/model.go
@@ -1,7 +1,10 @@
 package tstraining
 
 import (
-	"log"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
 
 	packetflow "github.com/kamilwoloszyn/cryptojacking-defender/models/packet-flow"
 )
@@ -36,17 +39,38 @@ func Extract(trafficStats *[]packetflow.TrafficStatistic) []TsTrainingData {
 			RecvMaliciousPacketRatio: getMaliciousPacketRatio(trafficItem.MaliciusTrafficStatistic.RecvKeywords, trafficItem.RecvQty),
 			AvgGapSentRT:             getAvgGapRT(trafficItem.FramesSendRelativeTime),
 			AvgGapRecvRT:             getAvgGapRT(trafficItem.FramesRecvRelativeTime),
-			AvgLenSentFrame:          getAvgLenFrame(trafficItem.FramesSendRelativeTime),
-			AvgLenRecvFrame:          getAvgLenFrame(trafficItem.FramesRecvRelativeTime),
-			SendRecvRatio:            getSendRecvRatio(&trafficItem),
+			AvgLenSentFrame:          getAvgLenFrame(trafficItem.FramesSendFrameLen),
+			AvgLenRecvFrame:          getAvgLenFrame(trafficItem.FramesRecvFrameLen),
+			SendRecvRatio:            getSendRecvRatio(trafficItem.SendQty, trafficItem.RecvQty),
 			ConsideredAs:             autoCompleteCryptoJackingState(&trafficItem),
 		})
 	}
 	return trainingData
 }
 
+// SaveAsJSON saves training data into a txt file
 func SaveAsJSON(data *[]TsTrainingData) error {
+	buf := new(bytes.Buffer)
+	if err := json.NewEncoder(buf).Encode(data); err != nil {
+		return fmt.Errorf(
+			"couldn't encode JSON: %v", err.Error(),
+		)
+	}
+	os.WriteFile("training_data.txt", buf.Bytes(), 0755)
 	return nil
+}
+
+func LoadFromJSON() ([]TsTrainingData, error) {
+	tData := []TsTrainingData{}
+	f, err := os.Open("training_data.txt")
+	if err != nil {
+		return []TsTrainingData{}, err
+	}
+	err = json.NewDecoder(f).Decode(&tData)
+	if err != nil {
+		return []TsTrainingData{}, err
+	}
+	return tData, nil
 }
 
 // AutoCompleteCryptoJackingState is a function that trying to consider if a field ConsideresAs should contain cryptojacking or nocryptojacking value.
@@ -60,17 +84,15 @@ func autoCompleteCryptoJackingState(statItem *packetflow.TrafficStatistic) Crypt
 	return FieldNonCryptoJackingBehavior
 }
 
-func getSendRecvRatio(statItem *packetflow.TrafficStatistic) float32 {
-	if statItem.RecvQty == 0 {
-		log.Println("[WARNING]: getSendRecvRatio: Unexpected recvQty zero value")
+func getSendRecvRatio(sentQty int, recvQty int) float32 {
+	if recvQty == 0 {
 		return 0
 	}
-	return float32(statItem.SendQty) / float32(statItem.RecvQty)
+	return float32(sentQty) / float32(recvQty)
 }
 
 func getMaliciousPacketRatio(mPacketQty int, packetQty int) float32 {
 	if packetQty == 0 {
-		log.Println("[WARNING]: getRecvMaliciousPacketRatio: Unexpected packetQty zero value")
 		return 0
 	}
 	return float32(mPacketQty) / float32(packetQty)
@@ -82,7 +104,7 @@ func getAvgGapRT(times []float32) float32 {
 		for i := 0; i < len(times)-1; i++ {
 			sumGap += getAbs(times[i] - times[i+1])
 		}
-		return sumGap / float32(len(times))
+		return sumGap / float32(len(times)-1)
 	}
 	if len(times) == 1 {
 		return times[0]
@@ -90,13 +112,13 @@ func getAvgGapRT(times []float32) float32 {
 	return 0
 }
 
-func getAvgLenFrame(items []float32) float32 {
+func getAvgLenFrame(items []int) float32 {
 	if len(items) > 0 {
-		var sum float32
+		var sum int
 		for _, item := range items {
 			sum += item
 		}
-		return sum / float32(len(items))
+		return float32(sum) / float32(len(items))
 	}
 	return 0
 }
