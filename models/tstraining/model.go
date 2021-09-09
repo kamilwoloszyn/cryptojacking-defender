53f4ee5fb8f0637639d820bc25dfb351a7e94951
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-09-09 20:27:28 +0200

    Updated model

--- models/tstraining/model.go
+++ models/tstraining/model.go
@@ -2,18 +2,14 @@ package tstraining
 
 import (
 	"bytes"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 
+	"github.com/kamilwoloszyn/cryptojacking-defender/models/base"
 	packetflow "github.com/kamilwoloszyn/cryptojacking-defender/models/packet-flow"
-)
-
-type CryptoJackingState string
-
-const (
-	FieldCryptoJackingBehavior    CryptoJackingState = "cyptojacking"
-	FieldNonCryptoJackingBehavior CryptoJackingState = "nocryptojacking"
 )
 
 // TsLearningData contains all data needed to machine learning
@@ -27,7 +23,8 @@ type TsTrainingData struct {
 	AvgLenSentFrame          float32
 	AvgLenRecvFrame          float32
 	SendRecvRatio            float32
-	ConsideredAs             CryptoJackingState
+	ConsideredAs             base.CryptoJackingState
+	EstimatedBehaviour       base.CryptoJackingState
 }
 
 // Extract extracts training data from PacketFlow struct
@@ -73,15 +70,108 @@ func LoadFromJSON() ([]TsTrainingData, error) {
 	return tData, nil
 }
 
+func SaveAsCSV(name string, data []TsTrainingData) error {
+	if name == "" {
+		return fmt.Errorf("no name specified")
+	}
+	if len(data) == 0 {
+		return fmt.Errorf("no data provided")
+	}
+	f, err := os.Create(os.TempDir() + "/" + name)
+	if err != nil {
+		return fmt.Errorf(
+			"could not create tmp file : %s", err.Error(),
+		)
+	}
+	writer := csv.NewWriter(f)
+	records := [][]string{
+		{
+			"sent_malicious_packet_ratio",
+			"recv_malicious_packet_ratio",
+			"avg_gap_sent_rt",
+			"avg_gap_recv_rt",
+			"avg_len_sent_frame",
+			"avg_len_recv_frame",
+			"send_recv_ratio",
+			"estimated_behaviour",
+		},
+	}
+	for _, trainingDataItem := range data {
+		records = append(records, []string{
+			fmt.Sprintf("%f", trainingDataItem.SentMaliciousPacketRatio),
+			fmt.Sprintf("%f", trainingDataItem.RecvMaliciousPacketRatio),
+			fmt.Sprintf("%f", trainingDataItem.AvgGapSentRT),
+			fmt.Sprintf("%f", trainingDataItem.AvgGapRecvRT),
+			fmt.Sprintf("%f", trainingDataItem.AvgLenSentFrame),
+			fmt.Sprintf("%f", trainingDataItem.AvgLenRecvFrame),
+			fmt.Sprintf("%f", trainingDataItem.SendRecvRatio),
+		})
+	}
+	writer.WriteAll(records)
+	return nil
+}
+
+// ReadFromCSV reads from given path, and returns Training data
+func ReadFromCSV(absPath string, containsHeader bool) ([]TsTrainingData, error) {
+	trainingData := []TsTrainingData{}
+	if absPath == "" {
+		return trainingData, fmt.Errorf("ReadFromCSV :path to file is empty")
+	}
+	file, err := os.Open(absPath)
+	if err != nil {
+		return trainingData, fmt.Errorf("ReadFromCSV: couldn't read file: %s", err.Error())
+	}
+	csvReader := csv.NewReader(file)
+	csvReader.FieldsPerRecord = -1
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		return trainingData, fmt.Errorf("ReadFromCSV: couldn't get records: %s", err.Error())
+	}
+	posX := 0
+	if containsHeader {
+		posX = 1
+	}
+	for ; posX < len(records); posX++ {
+		ok := true
+		sentMalPacRatio, err := strconv.ParseFloat(records[posX][0], 32)
+		ok = ok && err == nil
+		recvMaliciousPacketRatio, err := strconv.ParseFloat(records[posX][1], 32)
+		ok = ok && err == nil
+		avgGapSentRT, err := strconv.ParseFloat(records[posX][2], 32)
+		ok = ok && err == nil
+		avgGapRecvRT, err := strconv.ParseFloat(records[posX][3], 32)
+		ok = ok && err == nil
+		avgLenSentFrame, err := strconv.ParseFloat(records[posX][4], 32)
+		ok = ok && err == nil
+		avgLenRecvFrame, err := strconv.ParseFloat(records[posX][5], 32)
+		ok = ok && err == nil
+		sendRecvRatio, err := strconv.ParseFloat(records[posX][6], 32)
+		ok = ok && err == nil
+		if !ok {
+			return trainingData, fmt.Errorf("an error occured during parsing data (str -> float32)")
+		}
+		trainingData = append(trainingData, TsTrainingData{
+			SentMaliciousPacketRatio: float32(sentMalPacRatio),
+			RecvMaliciousPacketRatio: float32(recvMaliciousPacketRatio),
+			AvgGapSentRT:             float32(avgGapSentRT),
+			AvgGapRecvRT:             float32(avgGapRecvRT),
+			AvgLenSentFrame:          float32(avgLenSentFrame),
+			AvgLenRecvFrame:          float32(avgLenRecvFrame),
+			SendRecvRatio:            float32(sendRecvRatio),
+		})
+	}
+	return trainingData, nil
+}
+
 // AutoCompleteCryptoJackingState is a function that trying to consider if a field ConsideresAs should contain cryptojacking or nocryptojacking value.
 // The function is based on SentMaliciousPacketRatio and RecvMaliciousPacketRatio data.
 // If this data are greater than 0, then cryptojacking value will be applied.
 // Due to simple alghoritm use this with careful.
-func autoCompleteCryptoJackingState(statItem *packetflow.TrafficStatistic) CryptoJackingState {
+func autoCompleteCryptoJackingState(statItem *packetflow.TrafficStatistic) base.CryptoJackingState {
 	if statItem.MaliciusTrafficStatistic.RecvKeywords > 0 && statItem.MaliciusTrafficStatistic.SentKeywords > 0 {
-		return FieldCryptoJackingBehavior
+		return base.FieldCryptoJackingBehavior
 	}
-	return FieldNonCryptoJackingBehavior
+	return base.FieldNonCryptoJackingBehavior
 }
 
 func getSendRecvRatio(sentQty int, recvQty int) float32 {
