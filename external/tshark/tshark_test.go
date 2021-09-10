af031f36f629f2fd487c9e2a89baac44abbc491a
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-09-10 21:56:40 +0200

    Refactored tests

--- external/tshark/tshark_test.go
+++ external/tshark/tshark_test.go
@@ -6,27 +6,32 @@ import (
 	"github.com/kamilwoloszyn/cryptojacking-defender/external/tshark"
 )
 
-type pcapLocation string
-
-type testDecryptArg struct {
-	pcapLocation pcapLocation
-}
-
 const (
 	sslFilePath = "/home/kamil/Desktop/DiplomaWork/traffic/crypto-loot/keys2.txt"
 )
 
 func TestDecrypt(t *testing.T) {
 	tshakClient := tshark.New(sslFilePath)
-	tsharkArgs := []testDecryptArg{
+
+	testCases := []struct {
+		desc         string
+		pcapLocation string
+		outputJSON   string
+		wantErr      bool
+	}{
 		{
+			desc:         "existing files",
 			pcapLocation: "/home/kamil/Desktop/DiplomaWork/traffic/crypto-loot/traffic.pcap",
+			outputJSON:   "/home/kamil/Desktop/DiplomaWork/traffic/crypto-loot/decryped-json.txt",
+			wantErr:      false,
 		},
 	}
 
-	for _, tsharkItem := range tsharkArgs {
-		if err := tshakClient.Decrypt(string(tsharkItem.pcapLocation)); err != nil {
-			t.Fail()
-		}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			if err := tshakClient.Decrypt(tC.pcapLocation, tC.outputJSON); err != nil {
+				t.Fatalf("Got err: %v but expected err to be %v", err, tC.wantErr)
+			}
+		})
 	}
 }
