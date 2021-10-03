ed16e290d038370440197f393e0c940063f15714
kamil.woloszyn1@gmail.com <kamil.woloszyn1@gmail.com>
2021-10-03 22:34:06 +0200

    literal

--- external/tshark/client.go
+++ external/tshark/client.go
@@ -23,14 +23,14 @@ func New(
 }
 
 // Decrypt drcrypt traffic using a keys obtaines from browser and saves file to a specific location.
-func (t *Tshark) Decrypt(pcapLocation string, decrypedPathJSON string) error {
+func (t *Tshark) Decrypt(pcapLocation string, decryptedPathJSON string) error {
 	log.Printf("tshark -r %s -o 'tls.keylog_file: %s' -Px -Y http", pcapLocation, t.sslKeysPath)
 	cmd := exec.Command("/bin/bash", "-c", fmt.Sprintf("tshark -r %s -o \"tls.keylog_file: %s\" -Y tls -Px -T json -e ip.src -e ip.dst -e tls.record.content_type -e data-text-lines -e tls.record.content_type -e frame.number -e frame.len -e frame.time -e frame.time_relative -e text", pcapLocation, t.sslKeysPath))
 	data, _ := cmd.Output()
 	// if err != nil {
 	// 	return fmt.Errorf("%s caused a following error: %s", cmd.String(), err)
 	// }
-	if err := save(data, decrypedPathJSON); err != nil {
+	if err := save(data, decryptedPathJSON); err != nil {
 		return err
 	}
 	return nil
