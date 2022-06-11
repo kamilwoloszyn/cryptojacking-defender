5b5a359f1b9dd38d0822957d3266625dd7f61254
Kamil Wołoszyn <kamil.woloszyn1@gmail.com>
2022-06-11 19:08:54 +0200

    refactor to services

--- cmd/main.go
+++ cmd/main.go
@@ -8,9 +8,17 @@ import (
 	"time"
 
 	"github.com/caarlos0/env"
-	"github.com/kamilwoloszyn/cryptojacking-defender/adapter"
+	"github.com/kamilwoloszyn/cryptojacking-defender/adapter/chrome"
+	"github.com/kamilwoloszyn/cryptojacking-defender/adapter/tcpdump"
+	"github.com/kamilwoloszyn/cryptojacking-defender/adapter/tshark"
+	"github.com/kamilwoloszyn/cryptojacking-defender/app"
+	"github.com/kamilwoloszyn/cryptojacking-defender/app/dataprocessor"
+	"github.com/kamilwoloszyn/cryptojacking-defender/app/minerscan"
+	"github.com/kamilwoloszyn/cryptojacking-defender/app/packetflow"
+	"github.com/kamilwoloszyn/cryptojacking-defender/app/traffic"
+	"github.com/kamilwoloszyn/cryptojacking-defender/app/tstraining"
+	"github.com/kamilwoloszyn/cryptojacking-defender/app/wordlist"
 	"github.com/kamilwoloszyn/cryptojacking-defender/config"
-	"github.com/kamilwoloszyn/cryptojacking-defender/models"
 	"github.com/kamilwoloszyn/cryptojacking-defender/utils/cleanup"
 )
 
@@ -25,29 +33,53 @@ func main() {
 		log.Fatalf("[FATAL]: Cannot parse config: %s. Unexpected behavior may occur.", err)
 	}
 	log.Println("[INFO]: Config loaded succesfully")
+	var service *app.Service
 	tcpDumpCtx, cancelTcpDump := context.WithCancel(ctx)
-
-	appModules, err := models.InitializeModules(&cfg, &wg)
+	chromeService := chrome.New(cfg.BrowserSSLFilePath)
+	tcpdumpService := tcpdump.New(cfg.NetworkInterface, cfg.TcpDumpFilePath)
+	tsharkService := tshark.New(cfg.BrowserSSLFilePath)
+	dataProcessorService, err := dataprocessor.New(cfg.TrainingCSVPathRelative)
+	if err != nil {
+		log.Fatalf("couldn't initialize dataprocessor service")
+	}
+	wordListService := wordlist.NewWordList(cfg.WordlistFilePath)
+	wordList, err := wordListService.ParseFromFile()
+	if err != nil {
+		log.Fatalf("couldn't get a wordlist")
+	}
+	minerScannerService := minerscan.New(wordList)
+	dataTrainerService := tstraining.NewDataTrainer(cfg.TrainingCSVPathRelative, false, true, true, true)
+	trafficParserService := traffic.NewTrafficParser(cfg.TcpDumpFilePath)
+	trafficStatisticService := packetflow.NewTrafficStatisticsGenerator(service)
+	service = app.NewService(
+		chromeService,
+		tcpdumpService,
+		tsharkService,
+		dataProcessorService,
+		minerScannerService,
+		dataTrainerService,
+		trafficParserService,
+		trafficStatisticService,
+		wordListService,
+	)
 	if err != nil {
 		log.Fatalf("[FATAL]:Couldn't initialize some of internal modules: %s ", err.Error())
 	}
-	externalServices := adapter.Inititalize(&cfg)
-
 	// Capturing, decrypting traffic procedure
 	log.Println("[INFO]: Starting capturing ...\n [WARNING]: Password may be required for sudo")
-	externalServices.TcpDump().Capture(tcpDumpCtx, &wg)
+	service.TcpDumpCapture(tcpDumpCtx, &wg)
 	log.Println("[INFO]: Waiting for a browser .. ")
-	if err := externalServices.Chrome().Run(); err != nil {
+	if err := service.RunChrome(); err != nil {
 		log.Fatalf("[FATAL]: Couldn't run chrome browser: %s. Exiting.", err)
 	}
 	log.Println("[INFO]: Browser closed. Looking for dump program ...")
 	cancelTcpDump()
 	wg.Wait()
 	log.Println("[INFO]: Fixing corrupted package ...")
-	externalServices.TcpDump().FixBrokenPackage(ctx)
+	service.FixBrokenPackage(ctx)
 	log.Println("[INFO]: Decrypting traffic ...")
 	for i := 0; i < 5; i++ {
-		err := externalServices.Tshark().Decrypt(cfg.TcpDumpFilePath, cfg.ExternalServicesDecryptedJSON)
+		err := service.Decrypt(cfg.TcpDumpFilePath, cfg.ExternalServicesDecryptedJSON)
 		if err == nil {
 			break
 		}
@@ -57,21 +89,22 @@ func main() {
 	}
 
 	// Processing data
-	rawTraffic, err := appModules.ParseTrafficFromJSONFile(cfg.ExternalServicesDecryptedJSON)
+	rawTraffic, err := service.ParseFromJSONFile()
 	if err != nil {
 		log.Fatalf("[FATAL]:ParseTrafficFromJSONFile: %s\n", err.Error())
 	}
-	trafficStatistic, err := appModules.GenerateTrafficStatistcs(&rawTraffic)
+
+	trafficStatistic, err := service.Generate(&rawTraffic)
 	if err != nil {
 		log.Fatalf("[FATAL]:GenerateTrafficStatistcs: %s\n", err.Error())
 	}
 	// Training and learning
-	trainingData := appModules.GenerateTrainingData(&trafficStatistic)
+	trainingData := service.Extract(&trafficStatistic)
 
-	accurancy, err := appModules.DataProcessor().Initialize()
+	accurancy, err := service.ProcessTrainingData()
 	if err != nil {
 		log.Printf("[INFO]:Couldn't initialize data processor: %s\n", err.Error())
-		err := appModules.SaveTrainingData(trainingData, cfg.ProjectRootPath+cfg.TrainingCSVPathRelative, true, false)
+		err := service.SaveAsCSV(trainingData, cfg.ProjectRootPath+cfg.TrainingCSVPathRelative)
 		if err != nil {
 			log.Fatalf("[FATAL]: Couldn't save training data: %s", err.Error())
 		}
@@ -79,12 +112,12 @@ func main() {
 		os.Exit(0)
 	}
 	log.Printf("[INFO]:Accurancy of trained model: %f", accurancy)
-	appModules.SaveTrainingData(trainingData, cfg.CSVToPredict, false, true)
-	result, err := appModules.DataProcessor().Estimate(cfg.CSVToPredict)
+	service.SaveAsCSV(trainingData, cfg.CSVToPredict)
+	result, err := service.Estimate(cfg.CSVToPredict)
 	if err != nil {
 		log.Fatalf("[FATAL]:Couldn't predict a given data: %s", err.Error())
 	}
-	appModules.DataProcessor().PrintStatistic(trainingData, result)
+	service.PrintStatistic(trainingData, result)
 	log.Println("[INFO]: Cleaning files ...")
 	cleanup.RemoveAsCurrentUser(cfg.BrowserSSLFilePath, cfg.TcpDumpFilePath, cfg.CSVToPredict, cfg.ExternalServicesDecryptedJSON)
 
